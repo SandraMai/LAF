f43066afcdd58bf1c883bd309c004f29dea173a6
unknown <tobrolii@tlu.ee>
2020-06-08 11:46:05 +0300

    header koos

--- java_liina/header.php
+++ java_liina/header.php
@@ -1,13 +1,13 @@
 <div class="flex-row header-row"> 
 
-    <div class="logo"><a href="/oop_php/">LOGO</a></div>
+    <div class="logo"><a href="#">LAF</a></div>
     <div class="aside"></div>
     <ul class="ul flex-row header-list">
-        <li>hoiupaik</li>
-        <li>kaotatud</li>
-        <li>leitud</li>
-        <li>oksjon</li>
-        <li>KKK</li>
+        <li><a href="#">HOIUPAIK</a></li>
+        <li><a href="#">KAOTATUD</a></li>
+        <li><a href="#">LEITUD</a></li>
+        <li><a href="#">OKSJON</a></li>
+        <li><a href="#">KKK</a></li>
     </ul>
 
 </div>
