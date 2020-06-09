4ed15fa8ce7fd291e486c22dc2c98e94cd4d36b5
Anete Vaalu <anetevlu@tlu.ee>
2020-06-09 13:20:26 +0300

    bloop

--- koos/header.php
+++ koos/header.php
@@ -3,7 +3,7 @@
     <div class="logo"><a href="index.php">LAF</a></div>
     <div class="aside"></div>
     <ul class="ul flex-row header-list">
-        <li><a href="#">HOIUPAIK</a></li>
+        <li><a href="storage.php">HOIUPAIK</a></li>
         <li><a href="lost.php">KAOTATUD</a></li>
         <li><a href="found.php">LEITUD</a></li>
         <li><a href="auction.php">OKSJON</a></li>
---
+++ koos/pages/storage.php
@@ -0,0 +1,23 @@
+<?php
+    require('../head.php');
+?>
+<body>
+    <div class="main-flex header">
+        <div class="aside"></div>
+        <!-- HEADER -->
+        <div class="main-section">
+            <?php require('../header.php'); ?>
+        </div>
+        <div class="aside"></div>
+    </div>
+
+
+        <div class="main-section">
+            <!-- pealkiri  -->
+            <div class="flex-row"> 
+                <h1 class="title">HOIUPAIGAD</h1>
+            </div>
+        </div>
+    
+
+</body>
