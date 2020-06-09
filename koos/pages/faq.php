1e1a734210661a834e9e2decaae5ffcb35fce179
Sandra Maidla <ss22nnuu@tlu.ee>
2020-06-09 13:14:58 +0300

    faq

---
+++ koos/pages/faq.php
@@ -0,0 +1,16 @@
+<?php 
+
+require('../head.php'); 
+
+?>
+
+<div class="main-flex header">
+    <div class="aside"></div>
+
+    <!-- HEADER -->
+        <div class="main-section">
+            <?php require('../header.php'); ?>
+        </div>
+    <div class="aside"></div>
+
+</div>
