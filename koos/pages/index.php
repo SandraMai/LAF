aa74fd7e83872812dfc5fe3a5235feafea55b4d9
unknown <tobrolii@tlu.ee>
2020-06-09 10:29:43 +0300

    nupu link lisatud avalehele

--- koos/pages/index.php
+++ koos/pages/index.php
@@ -24,11 +24,11 @@ require('../head.php');
 
         <!-- PAGE BODY -->
         <div class="flex-column homeTextWrap"> 
-            <h3 class="homeElement">Lost and Found</h3>
+            <h3>Lost and Found</h3>
             <br>
-            <h4 class="homeElement">Siia veebisaidile võib lisada Tallinna Ülikoolis kaotatud esemeid või otsida leitute hulgast.</h4>
+            <h4>Siia veebisaidile võib lisada Tallinna Ülikoolis kaotatud esemeid või otsida leitute hulgast.</h4>
             <br>
-            <button>LEITUD ESEMEID SIRVIMA</button>
+            <a href="found.php">LEITUD ESEMEID SIRVIMA</a>
         </div><!--.flex-column-->
 
 
