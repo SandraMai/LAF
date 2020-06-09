63210d19a21aae749558234c8b09489aa7fca93c
Herman Petrov <hp355837@tlu.ee>
2020-06-09 15:59:58 +0300

    Lisatud new_offer.php sisu

---
+++ koos/pages/new_offer.php
@@ -0,0 +1,46 @@
+<?php require('../head.php'); 
+require("../functions/oksjon_functions.php");
+require("../../../../config_laf.php");
+/*require("functions_user.php");
+require("functions_main.php");
+require("functions_pic.php");*/
+$database = "if19_LAF";
+
+?>
+<body>
+    
+    <div class="main-flex header">
+        <div class="aside"></div>
+        <!-- HEADER -->
+        <div class="main-section">
+            <?php require('../header.php'); ?>
+        </div>
+        <div class="aside"></div>
+    </div>
+    
+    <div class="main-flex page-body">
+        <div class="aside"></div>
+        <div class="main-section">
+            <!-- HERO TEXT  -->
+            <div class="flex-row"> 
+                <h1 class="title">OKSJON</h1>
+            </div>
+
+            <!-- PAGE NUMBERS -->
+            <div class="flex-row"> 
+                <div class="aside"></div>
+                <div>                
+                    <?php
+                    echo "Tänane kuupäev " . date("Y/m/d") . "<br>";
+                    ?></div>
+            </div>
+            <!-- PAGE BODY -->
+            <div class="flex-row"> 
+                </div><!--.products -->
+        </div>
+        <div class="aside"></div>
+    </div>
+    <script src="../js/timer.js"></script>
+    <script src="../js/script.js"></script>
+    </body>
+    </html>
