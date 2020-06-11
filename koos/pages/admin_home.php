cea2b90beee08896ec413bc9710f3f398f950ec3
AnneliP88 <AnneliP88@users.noreply.github.com>
2020-06-11 19:25:58 +0300

    admini home nupud nüüd vasakus ääres + hover background nendel nuppudel

--- koos/pages/admin_home.php
+++ koos/pages/admin_home.php
@@ -16,34 +16,35 @@ if(isset($_GET["logout"])){
 
 ?>
 
+
+
 <body class="homeBody">
+    <div class="main-flex header">
+        <div class="aside"></div>
 
-<div class="main-flex header">
-    <div class="aside"></div>
-
-    <!-- HEADER -->
-    <div class="main-section">
-        <?php require('../header_admin.php'); ?>
-    </div>
-    <div class="aside"></div>
-
-</div><!--.main-flex-->
-
-<!-- IMAGE -->
-<div>
-    <div class="main-section adminHomeSection">
-
-        <!-- PAGE BODY -->
-        <div class="flex-column"> 
-            <a class="adminHomeButton" href="lost.php">KAOTATUD ESEMED</a>
-            <a class="adminHomeButton" href="found.php">LEITUD ESEMED</a>
-            <a class="adminHomeButton" href="auction.php">OKSJON</a>
-            <a class="adminHomeButton" href="auction.php">AEGUNUD KUULUUTSED</a>              
-            <a class="adminHomeButton" href="admin_settings.php">SEADED</a> 
+        <!-- HEADER -->
+        <div class="main-section">
+            <?php require('../header_admin.php'); ?>
         </div>
+        <div class="aside"></div>
 
-    </div><!--.main-section-->
-</div>
+    </div><!--.main-flex-->
+
+    <div class="main-flex page-body homeBody">
+        <div class="aside"></div>
+
+            <div class="main-section">
+
+                <div class="flex-column"> 
+                    <a class="adminHomeButton" href="lost.php">KAOTATUD ESEMED</a>
+                    <a class="adminHomeButton" href="found.php">LEITUD ESEMED</a>
+                    <a class="adminHomeButton" href="auction.php">OKSJON</a>
+                    <a class="adminHomeButton" href="auction.php">AEGUNUD KUULUUTSED</a>              
+                    <a class="adminHomeButton" href="admin_settings.php">SEADED</a> 
+                </div>
+            </div>
+        <div class="aside"></div>
+    </div>
 
 </body>
 </html>
