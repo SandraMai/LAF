3abebf915f30c200ccbfd8b17176b0acb535674c
Sandra Maidla <ss22nnuu@tlu.ee>
2020-06-11 13:17:10 +0300

    admini headerile lisatud lambised nimed et active class töötaks

--- koos/header_admin.php
+++ koos/header_admin.php
@@ -3,10 +3,10 @@
     <div><a class="logo"href="admin_home.php">LAF</a></div>
     <div class="aside"></div>
     <ul class="ul flex-row header-list">
-        <li><a href="">KAOTATUD</a></li>
-        <li><a href="">LEITUD</a></li>
-        <li><a href="">OKSJON</a></li>
-        <li><a href="">AEGUNUD</a></li>
+        <li><a href="jhfjdfhsj.php">KAOTATUD</a></li>
+        <li><a href="ppphp.php">LEITUD</a></li>
+        <li><a href="piipp.php">OKSJON</a></li>
+        <li><a href="ppep.php">AEGUNUD</a></li>
         <li><a href="admin_settings.php">SEADED</a></li>
         <li><a class="logOutLink" href="?logout=1">LOGI VÄLJA</a></li>
     </ul>
