1d119cf6b830220966572930d9c24fc37e3078d4
AnneliP88 <AnneliP88@users.noreply.github.com>
2020-06-15 14:20:03 +0300

    uued headeri otseteed, et toimiksid ka admini logIn lehel

--- koos/header.php
+++ koos/header.php
@@ -1,13 +1,13 @@
 <div class="flex-row header-row"> 
 
-    <div><a class="logo"href="index.php">LAF</a></div>
+    <div><a class="logo"href="../pages/index.php">LAF</a></div>
     <div class="aside"></div>
     <ul class="ul flex-row header-list">
-        <li><a href="storage.php">HOIUPAIGAD</a></li>
-        <li><a href="lost.php">KAOTATUD</a></li>
-        <li><a href="found.php">LEITUD</a></li>
-        <li><a href="auction.php">OKSJON</a></li>
-        <li><a href="faq.php">KKK</a></li>
+        <li><a href="../pages/storage.php">HOIUPAIGAD</a></li>
+        <li><a href="../pages/lost.php">KAOTATUD</a></li>
+        <li><a href="../pages/found.php">LEITUD</a></li>
+        <li><a href="../pages/auction.php">OKSJON</a></li>
+        <li><a href="../pages/faq.php">KKK</a></li>
         <div class="aside"></div>
     </ul>
 </div>
