a57aac8404a73d8d008c00419c256b03f1d6b4e2
Liina <tobrolii@tlu.ee>
2020-06-16 10:13:16 +0300

    mini-header css ja header/head muudetud

--- koos/header.php
+++ koos/header.php
@@ -1,13 +1,73 @@
-<div class="flex-row header-row"> 
-
-    <div><a class="logo"href="../pages/index.php">LAF</a></div>
+<div class="main-flex header">
     <div class="aside"></div>
-    <ul class="ul flex-row header-list">
-        <li><a href="../pages/storage.php">HOIUPAIGAD</a></li>
-        <li><a href="../pages/lost.php">KAOTATUD</a></li>
-        <li><a href="../pages/found.php">LEITUD</a></li>
-        <li><a href="../pages/auction.php">OKSJON</a></li>
-        <li><a href="../pages/faq.php">KKK</a></li>
-        <div class="aside"></div>
-    </ul>
-</div>
+
+        <!-- HEADER -->
+        <div class="main-section">
+            <div class="flex-row header-row"> 
+
+                <div><a class="logo"href="../pages/index.php">LAF</a></div>
+
+                <div class="aside"></div>
+
+                <ul class="ul flex-row header-list">
+                    <li><a href="../pages/storage.php">HOIUPAIGAD</a></li>
+                    <li><a href="../pages/lost.php">KAOTATUD</a></li>
+                    <li><a href="../pages/found.php">LEITUD</a></li>
+                    <li><a href="../pages/auction.php">OKSJON</a></li>
+                    <li><a href="../pages/faq.php">KKK</a></li>
+                    <div class="aside"></div>
+                </ul><!--.ul.flex-row.header-list-->
+            </div><!--.flex-row.header-row-->
+        </div><!--.main-section-->
+
+
+    <div class="aside"></div>
+</div><!--.main-flex.header-->
+
+
+<!-- MOBILE HEADER -->
+<div class="main-flex mobile-header">
+
+        <!-- HEADER -->
+        <div class="main-section">
+            <div class="flex-row header-row"> 
+
+                <div><img class="mobileMenuImage js-open-header" src="../images/menu.png" class="logo"></div>
+
+                <div class="aside"></div>
+
+            </div><!--.flex-row.header-row-->
+        </div><!--.main-section-->
+
+
+</div><!--.main-flex.header-->
+
+
+<div class="main-flex side-nav">
+    <div class="aside"></div>
+
+        <!-- HEADER -->
+        <div class="main-section">
+            <div class="flex-row header-row"> 
+                <div class="dialogBox__closeIcon js-hide-header">
+                    <div class="dialogBox__close"></div>
+                </div>
+
+                <div class="headerLogo"><a class="logo"href="../pages/index.php">LAF</a></div>
+
+                <div class="aside"></div>
+
+                <ul class="ul flex-row header-list">
+                    <li><a href="../pages/storage.php">HOIUPAIGAD</a></li>
+                    <li><a href="../pages/lost.php">KAOTATUD</a></li>
+                    <li><a href="../pages/found.php">LEITUD</a></li>
+                    <li><a href="../pages/auction.php">OKSJON</a></li>
+                    <li><a href="../pages/faq.php">KKK</a></li>
+                    <div class="aside"></div>
+                </ul><!--.ul.flex-row.header-list-->
+            </div><!--.flex-row.header-row-->
+        </div><!--.main-section-->
+
+
+    <div class="aside"></div>
+</div><!--.main-flex.header-->
