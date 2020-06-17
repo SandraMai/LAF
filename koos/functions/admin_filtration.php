1a495358d84dab840574885828778deae2ec5623
Herman Petrov <hp355837@tlu.ee>
2020-06-17 15:09:43 +0300

    removed fat code from lost admin

--- koos/functions/admin_filtration.php
+++ koos/functions/admin_filtration.php
@@ -4,42 +4,40 @@
         $notice = null;
         $page = 'lost.php';
         $conn = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+        $sqlStatementMain="SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
+        FROM LOST_ITEM_AD WHERE expired = 0 AND deleted = 0 ";
+        $sqlStatementCondition=null;
+        $sqlAfterStatements=" ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?";
         if($searchedName==null&&$searchedArea==null&&$searchedCategory==null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
-            
+            $sqlStatementCondition="";
         }else if($searchedName!=null&&$searchedArea==null&&$searchedCategory==null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND description LIKE'%{$searchedName}%' AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition=" AND description LIKE'%{$searchedName}%' ";
         }else if($searchedName==null&&$searchedArea!=null&&$searchedCategory==null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND deleted = 0  AND lost_place LIKE '%{$searchedArea}%' ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition="  AND lost_place LIKE '%{$searchedArea}%' ";
         }else if($searchedName==null&&$searchedArea==null&&$searchedCategory!=null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND description LIKE'%{$searchedName}%' AND CATEGORY_category_ID='{$searchedCategory}' AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition=" AND description LIKE'%{$searchedName}%' AND CATEGORY_category_ID='{$searchedCategory}' ";
             
         }else if($searchedName!=null&&$searchedArea!=null&&$searchedCategory==null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND CATEGORY_category_ID='{$searchedCategory}' AND lost_place LIKE '%{$searchedArea}%' AND description LIKE'%{$searchedName}%'   AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition=" AND CATEGORY_category_ID='{$searchedCategory}' AND lost_place LIKE '%{$searchedArea}%' AND description LIKE'%{$searchedName}%' ";
         }else if($searchedName==null&&$searchedArea!=null&&$searchedCategory!=null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND lost_place LIKE '%{$searchedArea}%' AND CATEGORY_category_ID='{$searchedCategory}' AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition=" AND lost_place LIKE '%{$searchedArea}%' AND CATEGORY_category_ID='{$searchedCategory}' ";
             
         }else if($searchedName!=null&&$searchedArea==null&&$searchedCategory!=null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND description LIKE'%{$searchedName}%' AND CATEGORY_category_ID='{$searchedCategory}' AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition=" AND description LIKE'%{$searchedName}%' AND CATEGORY_category_ID='{$searchedCategory}' ";
             
         }else if($searchedName!=null&&$searchedArea!=null&&$searchedCategory!=null){
-            $stmt = $conn->prepare("SELECT lost_post_ID, description, picture, lost_place, DATE_FORMAT(lost_date, '%d'), DATE_FORMAT(lost_date, '%c'), DATE_FORMAT(lost_date, '%Y') 
-            FROM LOST_ITEM_AD WHERE expired = 0 AND description LIKE'%{$searchedName}%' AND lost_place LIKE '%{$searchedArea}%' AND CATEGORY_category_ID='{$searchedCategory}' AND deleted = 0 ORDER BY lost_post_ID DESC LIMIT 3 OFFSET ?");
+            $sqlStatementCondition=" AND description LIKE'%{$searchedName}%' AND lost_place LIKE '%{$searchedArea}%' AND CATEGORY_category_ID='{$searchedCategory}' ";
             
         }
-        
+        $sqlStatementMain.=$sqlStatementCondition;
+        $sqlStatementMain.=$sqlAfterStatements;
+        $stmt=$conn->prepare($sqlStatementMain);
+        echo $conn->error;
         $stmt->bind_param("i", $offset);
         echo $conn->error;
         $stmt->bind_result($id, $description, $pic, $place, $day, $month, $year);
         $stmt->execute();
-            
+        $email = getBidEmail($id);
             while($stmt->fetch()){
                 if($pic=="puudub"){
                     if($place == null){
@@ -89,7 +87,7 @@
         $sqlStatementMain="SELECT found_item_ad_ID, description,DATE_FORMAT(found_date, '%d'), DATE_FORMAT(found_date, '%c'), DATE_FORMAT(found_date, '%Y'),picture,CATEGORY_category_ID,place_found, storage_place_name
         FROM FOUND_ITEM_AD JOIN STORAGE_PLACE ON FOUND_ITEM_AD.STORAGE_PLACE_storage_place_ID = STORAGE_PLACE.storage_place_ID WHERE expired=0  AND deleted = 0 ";
         $sqlStatementCondition=null;
-        $sqlStatementOrderByAndOffser=" ORDER BY found_item_ad_ID DESC LIMIT 3 OFFSET ?";
+        $sqlAfterStatements=" ORDER BY found_item_ad_ID DESC LIMIT 3 OFFSET ?";
         if($searchedName==null&&$searchedArea==null&&$searchedCategory==null&&$searchedStorage==null){
         $sqlStatementCondition="";
         }
@@ -113,26 +111,34 @@
         
         }elseif($searchedName!=null&&$searchedArea!=null&&$searchedCategory!=null&&$searchedStorage==null){
             $sqlStatementCondition=" AND place_found LIKE '%{$searchedArea}%' AND description LIKE '%{$searchedName}%' AND CATEGORY_category_ID LIKE '{$searchedCategory}' ";
+
         /////////////////////////////////////////////////////////TINGIMUSED KOOS SEARCHED STORAGEGA
         }elseif($searchedName==null&&$searchedArea==null&&$searchedCategory==null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%'";
+
         }elseif($searchedName==null&&$searchedArea==null&&$searchedCategory!=null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND  STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' AND CATEGORY_category_ID LIKE '{$searchedCategory}' ";
+
         }elseif($searchedName==null&&$searchedArea!=null&&$searchedCategory==null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND place_found LIKE '%{$searchedArea}%' AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' ";
+
         }elseif($searchedName!=null&&$searchedArea==null&&$searchedCategory==null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' AND description LIKE '%{$searchedName}%'  ";
+
         }elseif($searchedName!=null&&$searchedArea!=null&&$searchedCategory==null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND place_found LIKE '%{$searchedArea}%' AND description LIKE '%{$searchedName}%' AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' ";
+
         }elseif($searchedName!=null&&$searchedArea==null&&$searchedCategory!=null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' AND description LIKE '%{$searchedName}%' AND CATEGORY_category_ID LIKE '{$searchedCategory}' ";
+
         }elseif($searchedName==null&&$searchedArea!=null&&$searchedCategory!=null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND place_found LIKE '%{$searchedArea}%' AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' AND CATEGORY_category_ID LIKE '{$searchedCategory}' ";
+
         }elseif($searchedName!=null&&$searchedArea!=null&&$searchedCategory!=null&&$searchedStorage!=null){
             $sqlStatementCondition=" AND place_found LIKE '%{$searchedArea}%' AND description LIKE '%{$searchedName}%' AND CATEGORY_category_ID LIKE '{$searchedCategory}' AND STORAGE_PLACE_storage_place_ID LIKE '%{$searchedStorage}%' ";
         }
         $sqlStatementMain.=$sqlStatementCondition;
-        $sqlStatementMain.=$sqlStatementOrderByAndOffser;
+        $sqlStatementMain.=$sqlAfterStatements;
         $stmt=$conn->prepare($sqlStatementMain);
         echo $conn->error;
         $stmt->bind_param("i", $offset);
