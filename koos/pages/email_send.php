94b3e88fd79a751c278d35133771ad99a28cb9d0
Sandra Maidla <ss22nnuu@tlu.ee>
2020-06-17 09:31:06 +0300

    emaili saatmise leht

--- koos/pages/email_send.php
+++ koos/pages/email_send.php
@@ -1,4 +1,38 @@
 <?php 
-
+require('../head.php'); 
 
 ?>
+
+<body>
+
+
+<?php require('../header.php'); ?>
+
+
+<div class="main-flex page-body">
+    <div class="aside"></div>
+
+
+    <div class="main-section">
+
+        <!-- HERO TEXT  -->
+        <div class="flex-row"> 
+            <h1 class="title">EMAILI SAATMINE</h1>
+        </div>
+
+        <!-- PAGE BODY -->
+        <div class="clearfix-50"></div>
+
+            <div id="flex-row">
+
+            </div>
+
+    </div><!--.main-section-->
+
+
+    <div class="aside"></div>
+</div><!--.main-flex-->
+
+
+</body>
+</html>
