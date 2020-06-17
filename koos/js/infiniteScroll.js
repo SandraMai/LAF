9266b375730f29e2f61457527c52e8bee04c960f
Liina <tobrolii@tlu.ee>
2020-06-17 12:59:04 +0300

    timer uuesti peale ajax

--- koos/js/infiniteScroll.js
+++ koos/js/infiniteScroll.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
                     $('.js-more-wrapper').empty().append('<h1 >Rohkem esemeid ei ole!</h1>');
                 } else {
                     createProductHTML(JSON.parse(data));
+                    hermaniTimer();
                 }
 
             });
@@ -46,5 +47,54 @@ $(document).ready(function() {
         $( data ).appendTo( $( ".products" ) );
     }
 
+
+
+    function hermaniTimer() {
+        $('.productexplinationsDATE').each(function(){
+            var compareDate = new Date();
+            console.log($(this).text());
+
+            let that = $(this);
+            // kui teed html selliseks
+            // <li data-time="1589922000" class="productexplinationsDATE">Oksjoni aegumiskuupäev: 1589922000</li>
+            // saad niimoodi ainult value, ja mitte innerhtml
+            let value = $(this).data('time');
+            value=value*1000+86400*14*1000
+
+            console.log("VALUE "+value)
+            var timer;
+            
+                timer = setInterval(function() {
+                    timeBetweenDates(that);
+                }, 1000);
+
+                function timeBetweenDates(that) {
+                    var now = new Date();
+                    var difference = value - now.getTime();
+
+                    if (difference <= 0) {
+
+                        // Timer done
+                        clearInterval(timer);
+                    
+                    } else {
+                        
+                        var seconds = Math.floor(difference / 1000);
+                        var minutes = Math.floor(seconds / 60);
+                        var hours = Math.floor(minutes / 60);
+                        var days = Math.floor(hours / 24);
+
+                        hours %= 24;
+                        minutes %= 60;
+                        seconds %= 60;
+
+                        $(that).find('.days').text(days);
+                        $(that).find('.hours').text(hours);
+                        $(that).find('.minutes').text(minutes);
+                        $(that).find('.seconds').text(seconds);
+                    }
+                }
+        });
+    }
 
 });
