ad6af6fa6a903ac717b4eeb127f6c22872904ba2
Sandra Maidla <ss22nnuu@tlu.ee>
2020-06-19 09:36:52 +0300

    viimane muudatus

--- koos/functions/admin_filtration.php
+++ koos/functions/admin_filtration.php
@@ -48,7 +48,7 @@
                         $place = "Kaotamise koha kohta info puudub!";
                     }
                     $notice .= ' <div class="product">';
-                    $notice .= '<a class="productImageBox" href="admin_view_ad.php?id=' .$id .'"><img class="productImage" src="' .$GLOBALS["pic_read_dir_thumb"] .$pic .'"></a>';
+                    $notice .= '<a class="productImageBox" href="admin_view_ad.php?id=' .$id .'"><img class="productImage" src="../images/missing.png"></a>';
                     $notice .= '<div class="productDesc">';
                     $notice .= '<p> Kirjeldus: ' .$description .'</p>';
                     $notice .= '<p>Kaotamise koht: ' .$place .'</p>';
